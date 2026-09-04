621fae2f1054b4e712d335365e31e1cd4b1ead40
testingchance1 <testingchance1@gmail.com>
2026-09-04 11:30:42 -0500

    added docs folder with step 1 for azure, added bash/powershell scripts for initial storage setup for terraform

---
+++ cicd-terraform-environment/utilities/bash/az_initial_storage_setup.sh
@@ -0,0 +1,34 @@
+# variables you can customize to your liking, think about your naming convention that makes sense to you
+RESOURCE_GROUP_NAME="rg-terraform-state"
+STORAGE_ACCOUNT_NAME="stterraformstate$(openssl rand -hex 4)" #this value must be globally unique so we use a random generator to add some numbers at the end
+CONTAINER_NAME="tfstate"
+LOCAITON="centralus" #pick a location close to you, pricing does vary though, keep it in mind
+
+#create a resource group
+az group create \
+  --name $RESOURCE_GROUP_NAME \
+  --locaiton $LOCATION
+
+#create storage account
+az storeage account create \
+  --name $STORAGE_ACCOUNT_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --location $LOCATION \
+  --sku Standard_LRS
+  --encryption-services blob \
+  --min-tls-version TLS1_2 \
+  --allow-blob-public-access false
+
+#create blob container
+az storage container create \
+ --name $CONTAINER_NAME \
+ --account-name $STORAGE_ACCOUNT_NAME
+
+#enable versioning for state file recovery
+az storage account blob-service-properties update \
+  --account-name $STORAGE_ACCOUNT_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --enable-versioning true
+
+echo "Storage Account Name: $STORAGE_ACCOUNT_NAME"
+echo "Save this name - you will need it for your .env file"
